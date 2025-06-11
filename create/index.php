64b99cb5e5618b4c67207c526351cafda55cba02
matheus dev <50026803+MTHS1901@users.noreply.github.com>
2025-06-11 14:45:29 -0300

    Update index.php
    Signed-off-by: matheus dev <50026803+MTHS1901@users.noreply.github.com>

--- create/index.php
+++ create/index.php
@@ -4,16 +4,17 @@
 include("../config.php");
 
 // Dados recebidos via POST
-$name = strtolower($_POST['name']);
-$email = strtolower($_POST['email']);
-$value = $_POST['value'];
-// fixes
-$value = str_replace(",", ".", $value);
-$value = floatval($value);
+$name = strtolower($_POST['name']); // nome do usuário
+$email = strtolower($_POST['email']); // email do usuário
+$value = $_POST['value']; // valor da compra
 
+// fix value
+$value = floatval(str_replace(",", ".", $value));
+
+// descrição/nome do produto
 $description = "PRODUTO #1";
 
-// pagador
+// monta pagador
 $pagador = [
     "first_name" => $name,
     "last_name" => "",
@@ -25,30 +26,23 @@ $externalReference = geraString(24);
 
 // função para gerar a string
 function geraString($length) {
-    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
+    return substr(str_shuffle(str_repeat('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', $length)), 0, $length);
 }
 
-
-// Informações sobre o pagamento:
+// Informações sobre o pagamento
 $infos = [
     "notification_url" => $notification_url, // config.php
-    "description" => $description,
-    "external_reference" => $externalReference,
-    "transaction_amount" => $value,
-    "payment_method_id" => "pix"
+    "description" => $description, // post
+    "external_reference" => $externalReference, // post
+    "transaction_amount" => $value, // post
+    "payment_method_id" => "pix" // altera manualmente
 ];
 
 // encoda as informações em json
 $payment = array_merge(["payer" => $pagador], $infos);
 $payment = json_encode($payment);
 
-// 
+// gera um ID único (mercado pago exige isso)
 $uuid = generateUUID();
 function generateUUID()
 {
@@ -79,17 +73,18 @@ curl_setopt_array($curl, [
     ]
 ]);
 
+// resposta
 $response = curl_exec($curl);
 curl_close($curl);
 
-// decoda a response
+// decoda a resposta
 $data = json_decode($response, true);
 $response = json_decode($response, true);
 
 // simplifica o caminho para a key com os dados da transação
 $response = $response['point_of_interaction']['transaction_data'];
 
-// cria uma array apenas com as informações
+// cria uma array apenas com as informações do pagamento
 $arr = array('qr_code' => $response['qr_code'], 'qr_code_base64' => $response['qr_code_base64'], 'payment_url' => $response['ticket_url'], 'id' => $data['id'], 'ref' => $externalReference, 'full_info_for_developer' => $data);
 
 // cria um arquivo com a referencia da transação para verificar se o pagamento foi aprovado depois
