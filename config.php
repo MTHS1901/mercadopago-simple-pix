67cbc36a83098742f208751e9cc89bda54e22c9e
matheus dev <50026803+MTHS1901@users.noreply.github.com>
2025-06-11 14:48:22 -0300

    Update config.php
    Signed-off-by: matheus dev <50026803+MTHS1901@users.noreply.github.com>

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 // Código por: github.com/mths1901
 
-date_default_timezone_set('America/Sao_Paulo');
+header('Access-Control-Allow-Origin: *'); // permita receber requisições de qualquer dominio para todos os arquivos /create /verify etc...
+
+date_default_timezone_set('America/Sao_Paulo'); // seta o timezone para America/Sao_Paulo
 
 // https://www.mercadopago.com.br/settings/account/credentials
 $acess_token = "SEU_ACESS_TOKEN";
 
 // o mercadopago envia uma notifição toda vez que houver uma mudança na transação, este seria seu URL com o script para receber essa notificação
-$notification_url = "CAMINHO_PARA_NOTIFICATION"; // ***obrigatorio***
+$notification_url = "CAMINHO_PARA_NOTIFICATION"; // ***obrigatorio*** - exemplo "https://seudominio.com/api/notification/"
 ?>
